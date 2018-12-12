bd122af53571da37e8cc270c9ee58e2a0126c98d
christine.pham <cstinepham@gmail.com>
2018-12-11 17:33:05 -0800

    Implement SessionAPIUtil

---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,26 @@
+export const signup = user => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/users',
+    data: {
+      user
+    }
+  })
+);
+
+export const login = user => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/session',
+    data: { user }
+  })
+);
+
+
+
+export const logout = () => (
+  $.ajax({
+    method: 'DELETE',
+    url: '/api/session'
+  })
+);
