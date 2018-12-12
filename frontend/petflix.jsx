63a6c8a5b7bdd2f61bab4c55216d0b48f2e14424
christine.pham <cstinepham@gmail.com>
2018-12-11 22:12:26 -0800

    Refactor out App and Root to include HashRouter

--- frontend/petflix.jsx
+++ frontend/petflix.jsx
@@ -2,15 +2,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
-
+import Root from "./components/root";
 
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  
   let store = configureStore();
+
+  // TESTING START
   window.getState = store.getState;
-  window.dispatch = store.dispatch; // just for testing!
+  window.dispatch = store.dispatch;
+  // TESTING END
+
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to petflix!</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
