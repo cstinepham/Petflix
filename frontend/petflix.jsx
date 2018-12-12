6684c212dc508727644ca0034ea35ac83b2c2f6e
christine.pham <cstinepham@gmail.com>
2018-12-11 22:01:03 -0800

    Create store and test that state shape is working in console

--- frontend/petflix.jsx
+++ frontend/petflix.jsx
@@ -1,9 +1,16 @@
 //React
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  
+  let store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch; // just for testing!
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to petflix!</h1>, root);
 });
