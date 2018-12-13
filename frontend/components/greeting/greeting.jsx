99bd8bd808eb736f33271d086c91503cf4bd2732
christine.pham <cstinepham@gmail.com>
2018-12-13 00:35:38 -0800

    Add more styling and login button to greeting page

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -3,23 +3,23 @@ import React from 'react';
 class Greeting extends React.Component {
 
   render() {
-    return (
-      <div className="splash-background">
-        <div>
-          
+    return <div className="splash-background">
+        <div className="splash-navbar">
+          <div className="splash-logo"/>
+        <button className="login-button">Sign In</button>
         </div>
-        
         <div className="splash-card-1">
           <div className="splash-card-1-text">
-            <h1>
-              Watch on any device
-            </h1>
+            <h1>Watch on any device</h1>
+            <p>
+              Stream on your phone, tablet, laptop, and TV without paying
+              more.
+            </p>
+            <button className="signup-button">JOIN FREE FOR A MONTH</button>
           </div>
-          <div className="splash-card-1-img">
-          </div>
+          <div className="splash-card-1-img" />
         </div>
-      </div> 
-    );
+      </div>;
   }
   
 }
