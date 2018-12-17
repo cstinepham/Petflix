9656bc7e7aec747a934bec4b4663d9be240ec1d1
christine.pham <cstinepham@gmail.com>
2018-12-17 12:03:19 -0800

    Redirect clicking on moviegenrelist item to watch movie page

--- frontend/components/movie_genre_list/movie_genre_item.jsx
+++ frontend/components/movie_genre_list/movie_genre_item.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const MovieGenreItem = props => (
   <div className="tile">
     <div className="tile__media">
       <img className="tile__img" src={props.movie.photo} alt="" />
     </div>
-    <div className="tile__details">
-      <div className="tile__title">
-        {props.movie.title} 
-        <br></br>
-        {props.movie.description}
-      </div>
-    </div>
+      <Link to={`/watch/${props.movie.id}`}>
+        <div className="tile__details">
+          <div className="tile__title">
+            {props.movie.title} 
+            <br></br>
+            {props.movie.description}
+          </div>
+        </div>
+      </Link>
   </div>
 );
 
