620fce4d8bc8995b1a4d1656472fdf165094cb1e
christine.pham <cstinepham@gmail.com>
2018-12-20 01:13:16 -0800

    Add more styling to creating a new list functionality

--- frontend/components/list/lists.jsx
+++ frontend/components/list/lists.jsx
@@ -15,28 +15,37 @@ class Lists extends React.Component {
     return <div>
         <div className="my-lists">My Lists</div>
         <div className="grid-container">
+          <div className="modal-container">
+            <input id="modal-toggle" type="checkbox" />
+            <label className="modal-btn" htmlFor="modal-toggle">
+              <strong>+</strong>
+            </label>
+            <label className="modal-backdrop" htmlFor="modal-toggle" />
+            <div className="modal-content">
+              <label className="modal-close" htmlFor="modal-toggle">
+                &#x2715;
+              </label>
+              <div className="create-list">Create New Movie List</div>  <hr />
 
-        <div className="modal-container">
-          <input id="modal-toggle" type="checkbox" />
-          <label className="modal-btn" htmlFor="modal-toggle">+</label>
-          <label className="modal-backdrop" htmlFor="modal-toggle"></label>
-          <div className="modal-content">
-            <label className="modal-close" htmlFor="modal-toggle">&#x2715;</label>
-            <h2>Create List</h2>
-            <p>Hello from inside the modal!</p>
-            <label className="modal-content-btn" htmlFor="modal-toggle">OK</label>
+              <form>
+                <input className="title-input" type="text" placeholder="ex. Good Crying Favs"/>
+                <br></br>
+                
+              <input className="modal-content-btn" htmlFor="modal-toggle" type="submit" value="Submit"/>
+   
+                      
+              </form>
+{/*            
+              <label className="modal-content-btn" htmlFor="modal-toggle">
+                OK
+              </label> */}
+            </div>
           </div>
-        </div>  
 
-          
           <div className="list" />
           <div className="list" />
           <div className="list" />
         </div>
-
-
-
-
       </div>;
   }
 }
